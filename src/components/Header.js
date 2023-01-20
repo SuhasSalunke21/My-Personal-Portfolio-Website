060a123c577d3e46d0b21a61e38fd742f5452e9a
mvpatil45 <94850385+mvpatil45@users.noreply.github.com>
2023-01-21 00:14:16 +0530

    Added logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import "./Header.css";
+import logo from "../assests/favicon.png"
 export default function header() {
   return (
     <div className="header">
-        <Link to="/">Portfolio</Link>
+        <Link to="/"><img src={logo} alt="LOGO" style={{width: "10%" ,marginRight:"1rem"}}></img>Portfolio</Link>
         <ul className='NavMenu'>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/projects">Projects</Link></li>
